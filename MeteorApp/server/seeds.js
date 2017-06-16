a7a2a670c11c139912f170af0760e66fc976d043
Connor Larkin <connor.larkin1@gmail.com>
2017-06-16 00:07:53 -0500

    switched to angular JS and updated comment panel seed gen

--- MeteorApp/server/seeds.js
+++ MeteorApp/server/seeds.js
@@ -1,5 +1,6 @@
 import Details from '/lib/collections/details';
 import Posts from '/lib/collections/posts';
+//meteor reset to wipe DB
 
 const seed = () => {
   if (Details.find().count() === 0) {
@@ -11,20 +12,12 @@ const seed = () => {
   }
 
   if (Posts.find().count() === 0) {
-    Posts.insert({
-      title: 'Lorem ipsum dolor sit',
-      body: 'Lorem ipsum dol d'
-    });
-
-    Posts.insert({
-      title: 'Meteor Lorem ipsum dolor sit',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
-    });
-
-    Posts.insert({
-      title: 'The Lorem ipsum dolor sit',
-      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut '
-    });
+    for (let i = 0; i < 100; i++) {
+      Posts.insert({
+        title: 'Meteor test post #'+i,
+        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      });
+    }
   }
 
 
