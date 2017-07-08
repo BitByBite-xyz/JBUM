3d7860358174377193a0fe34ce67b99eb30a4d4b
Connor Larkin <connor.larkin1@gmail.com>
2017-07-07 22:40:29 -0500

    hih

--- RNApp/app/components/AskHeader/styles.js
+++ RNApp/app/components/AskHeader/styles.js
@@ -65,7 +65,6 @@ export default StyleSheet.create({
         paddingBottom: 10,
         paddingTop: 15,
         paddingLeft: 17,
-        fontSize: 10,
         justifyContent: 'center',
         alignItems: 'center'
     },
@@ -87,7 +86,6 @@ export default StyleSheet.create({
     },
     lineDivider: {
         width: 320,
-        color: '#BABABA',
         borderTopColor: '#DBD9D9',
         borderTopWidth: .5,
         paddingLeft: 10,
