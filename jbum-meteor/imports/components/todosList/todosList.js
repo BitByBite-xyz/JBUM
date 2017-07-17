6cfdbf80ec34725fb444d1aa4408116bec9fe264
youngjin17 <youngjin.w.kim@gmail.com>
2017-07-17 11:20:06 -0500

    fixed mongo shit

--- jbum-meteor/imports/components/todosList/todosList.js
+++ jbum-meteor/imports/components/todosList/todosList.js
@@ -2,19 +2,72 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './todosList.html';
 import { Tasks } from '../../api/tasks.js';
+import { Meteor } from 'meteor/meteor';
+
 
 class TodosListCtrl {
-  constructor($scope) {
-    $scope.viewModel(this);
+    constructor($scope) {
+      $scope.viewModel(this);
 
-    this.helpers({
-      tasks() {
-        return Tasks.find({});
+      this.hideCompleted = false;
+
+      this.helpers({
+        tasks() {
+          const selector = {};
+
+          // If hide completed is checked, filter tasks
+          if (this.getReactively('hideCompleted')) {
+            selector.checked = {
+              $ne: true
+            };
+        }
+
+          // Show newest tasks at the top
+          return Tasks.find(selector, {
+            sort: {
+                createdAt: -1
+              }
+            });
+          },
+          incompleteCount() {
+            return Tasks.find({
+              checked: {
+                $ne: true
+              }
+            }).count();
+        },
+        currentUser() {
+          return Meteor.user();
       }
-    })
+  })
   }
-}
 
+       addTask(newTask) {
+         // Insert a task into the collection
+         Tasks.insert({
+           text: newTask,
+           createdAt: new Date,
+           owner: Meteor.userId(),
+           username: Meteor.user().username
+       });
+
+         // Clear form
+         this.newTask = '';
+       }
+
+ setChecked(task) {
+   // Set the checked property to the opposite of its current value
+   Tasks.update(task._id, {
+     $set: {
+       checked: !task.checked
+     },
+   });
+ }
+
+ removeTask(task) {
+   Tasks.remove(task._id);
+ }
+ }
 export default angular.module('todosList', [
   angularMeteor
 ])
--- jbum-meteor/server/main.js
+++ jbum-meteor/server/main.js
@@ -1,3 +1,7 @@
-// Server entry point, imports all server code
-
+import { Meteor } from 'meteor/meteor';
 import '../imports/api/tasks.js';
+import { Accounts } from 'meteor/accounts-base';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
