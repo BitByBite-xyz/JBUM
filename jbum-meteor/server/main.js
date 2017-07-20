246b0e1df00e9721c2cec454d9bba6b30367007c
Connor Larkin <connor.larkin1@gmail.com>
2017-07-19 22:11:20 -0500

    fixes main.js

--- jbum-meteor/server/main.js
+++ jbum-meteor/server/main.js
@@ -1,7 +1,2 @@
-import { Meteor } from 'meteor/meteor';
-import '../imports/api/tasks.js';
-import { Accounts } from 'meteor/accounts-base';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
+import '../imports/startup/server';
+import '../imports/startup/both';
