6363767fc8de12aa27e065dd55dc1d5ec7f321f1
Connor Larkin <connor.larkin1@gmail.com>
2017-07-25 14:06:42 -0500

    upd8

--- jbum-react-native/index.android.js
+++ jbum-react-native/index.android.js
@@ -2,3 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './app/index';
 
 AppRegistry.registerComponent('jbum-react-native', () => App);
+
+
+///Users/connorlarkin/Library/Android/sdk
