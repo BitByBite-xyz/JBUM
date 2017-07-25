6982274b0c3b92f53bf9adb241a0fcd4403db023
Connor Larkin <connor.larkin1@gmail.com>
2017-07-25 14:27:35 -0500

    rename again to fix android!

--- jbum-react-native/index.android.js
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './app/index';
-
-AppRegistry.registerComponent('jbum-react-native', () => App);
-
-
-///Users/connorlarkin/Library/Android/sdk
