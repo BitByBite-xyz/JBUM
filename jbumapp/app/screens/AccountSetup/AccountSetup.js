e28938b0e04e086a6a832ed2d4958a2afb7d2e14
prinse1545 <philipp.msrivastava@gmail.com>
2017-08-09 17:34:06 -0500

    Added basic screens

--- jbumapp/app/screens/AccountSetup/AccountSetup.js
+++ jbumapp/app/screens/AccountSetup/AccountSetup.js
@@ -7,6 +7,7 @@
    	Linking,
     TouchableOpacity,
     Alert,
+    StyleSheet,
 }
 from 'react-native';
 import Meteor, { Accounts } from 'react-native-meteor';
@@ -15,13 +16,50 @@ import {
 	Icon
 }
 from 'react-native-elements';
+import AppIntro from 'react-native-app-intro';
 
 
 export default class AccountSetup extends Component {
 
   render() {
     return(
-      <Text>Hello</Text>
+      <AppIntro>
+        <View style={[styles.slide,{ backgroundColor: '#fa931d' }]}>
+          <View level={10}><Text style={styles.text}>Page 1</Text></View>
+          <View level={15}><Text style={styles.text}>Page 1</Text></View>
+          <View level={8}><Text style={styles.text}>Page 1</Text></View>
+        </View>
+        <View style={[styles.slide, { backgroundColor: '#a4b602' }]}>
+          <View level={-10}><Text style={styles.text}>Page 2</Text></View>
+          <View level={5}><Text style={styles.text}>Page 2</Text></View>
+          <View level={20}><Text style={styles.text}>Page 2</Text></View>
+        </View>
+        <View style={[styles.slide,{ backgroundColor: '#fa931d' }]}>
+          <View level={8}><Text style={styles.text}>Page 3</Text></View>
+          <View level={0}><Text style={styles.text}>Page 3</Text></View>
+          <View level={-10}><Text style={styles.text}>Page 3</Text></View>
+        </View>
+        <View style={[styles.slide, { backgroundColor: '#a4b602' }]}>
+          <View level={5}><Text style={styles.text}>Page 4</Text></View>
+          <View level={10}><Text style={styles.text}>Page 4</Text></View>
+          <View level={15}><Text style={styles.text}>Page 4</Text></View>
+        </View>
+      </AppIntro>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+    padding: 15,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
