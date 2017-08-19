9f3053e0717bb976d21b5f37c477ca69b44882a6
Sussbus <jacobsussman5@gmail.com>
2017-08-18 21:01:05 -0500

    made stats component more simple

---
+++ jbummeteor/imports/ui/components/StatsCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Paper from 'material-ui/Paper';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+const StatsCard = (props) => {
+  const { cardTitle , cardDiscriptor, cardStyle} = props;
+
+  return (
+        <div className="col-sm-4 row-no-padding">
+          <Paper zDepth={1}>
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+              <div style={cardStyle}/>
+              <div style={{marginLeft: '15%', marginTop: '5.5%'}}>
+                <p className="dashboardCardNumber" style={{fontSize: 32}}>{cardTitle}<br /></p>
+                 <p style={{fontSize: 17, color: 'gray'}}>{cardDiscriptor}</p>
+              </div>
+            </div>
+          </Paper>
+        </div>
+      );}
+
+export default StatsCard;
