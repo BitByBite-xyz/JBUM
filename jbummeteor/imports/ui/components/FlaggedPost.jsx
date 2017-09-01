eb4ef717066d94a2b465248dca9539064da5db5b
Sussbus <jacobsussman5@gmail.com>
2017-08-31 21:56:55 -0500

    bug fixes

--- jbummeteor/imports/ui/components/FlaggedPost.jsx
+++ jbummeteor/imports/ui/components/FlaggedPost.jsx
@@ -28,7 +28,7 @@ const FlaggedPost = (props) => {
   return (
     <div className="col-sm-4 flaggedPostContainer" style={{marginBottom: 15}}>
       <Paper zDepth={1} style={{borderRadius: 5, paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
-        <div className="flaggedPostContent" style={{ height: 150, textOverflow: 'elipsis'}}>
+        <div className="flaggedPostContent" style={{ height: 150, overflow: 'scroll'}}>
           <p style={{fontSize: 18}}>{postContent.post_title}</p>
           <Divider style={{width: '95%', marginBottom: 10, marginLeft: '2.5%'}}/>
           <p>{postContent.post_body}</p>
