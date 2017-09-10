87c8e727da21887cf7cee448647a7bd8c1c944b3
Chris <chriscallanjr@me.com>
2017-09-09 20:35:10 -0400

    Finally squashed this damn keyboard bug, several clones later

--- jbumapp/app/screens/Reply/Reply.js
+++ jbumapp/app/screens/Reply/Reply.js
@@ -6,12 +6,14 @@ import {
   TouchableOpacity,
   FlatList,
   Keyboard,
-  LayoutAnimation
+  LayoutAnimation,
+  KeyboardAvoidingView
 } from 'react-native';
 import React, { Component } from 'react';
 import { Button, Icon } from 'react-native-elements'
 import moment from 'moment';
 import {AutoGrowingTextInput} from 'react-native-autogrow-textinput';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import styles from './styles'
 import Comment from './Comment'
@@ -20,57 +22,57 @@ import Meteor, { createContainer } from 'react-native-meteor';
 const Reply = (props) => {
   const { navigation, replyButton, body,updateState,post } = props;
    return (
-     <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainerStyle}
-     >
-       <View style={styles.backdrop}>
-           <View style={styles.topPadding}></View>
-           <View style={styles.topBox}>
-               <View style={styles.questionTitleContainer}>
-                   <Text style={styles.questionTitleText}>{post.post_title}</Text>
-                   <View style={styles.lineDivider} />
-               </View>
-               <Text style={styles.questionText}>{post.post_body}</Text>
-               <Text style={[styles.text, styles.created]}>{moment(post.createdAt).fromNow()}</Text>
-           </View>
-           <View style={styles.views}>
-              {post.user_id === Meteor.userId() ? post.post_comments.map((comment) => (
-                <Comment
-                  key={comment.comment_id}
-                  postComment={comment}
-                />
-              )):null}
-            </View>
-        <View style={styles.bottomWrapper}>
-           <View style={styles.bottomBox}>
-               <View style={styles.bottom}>
-                   <View style={styles.views}>
-                       <AutoGrowingTextInput
-                           style={styles.largeText}
-                           placeholder='Reply to this question'
-                           returnKeyType="done"
-                           placeholderTextColor='#DBD9D9'
-                           underlineColorAndroid='transparent'
-                           onChangeText={(body) => updateState( {body} )}
-                           blurOnSubmit={true}
-                           multiline={true}
-                           minHeight={60}
-                           maxHeight={200}
-                           autoCorrect={true}
-                       />
+         <KeyboardAwareScrollView
+            style={styles.container}
+            contentContainerStyle={styles.contentContainerStyle}
+         >
+           <View style={styles.backdrop}>
+               <View style={styles.topPadding}></View>
+               <View style={styles.topBox}>
+                   <View style={styles.questionTitleContainer}>
+                       <Text style={styles.questionTitleText}>{post.post_title}</Text>
+                       <View style={styles.lineDivider} />
                    </View>
-                <View style={styles.buttonView}>
-                   <View style={styles.lineDivider}/>
-                    <TouchableOpacity onPress={replyButton}>
-                      <Text style={styles.button}>Submit Reply</Text>
-                    </TouchableOpacity>
+                   <Text style={styles.questionText}>{post.post_body}</Text>
+                   <Text style={[styles.text, styles.created]}>{moment(post.createdAt).fromNow()}</Text>
+               </View>
+               <View style={styles.views}>
+                  {post.user_id === Meteor.userId() ? post.post_comments.map((comment) => (
+                    <Comment
+                      key={comment.comment_id}
+                      postComment={comment}
+                    />
+                  )):null}
+                </View>
+            <View style={styles.bottomWrapper}>
+               <View style={styles.bottomBox}>
+                   <View style={styles.bottom}>
+                       <View style={styles.views}>
+                           <AutoGrowingTextInput
+                               style={styles.largeText}
+                               placeholder='Reply to this question'
+                               returnKeyType="done"
+                               placeholderTextColor='#DBD9D9'
+                               underlineColorAndroid='transparent'
+                               onChangeText={(body) => updateState( {body} )}
+                               blurOnSubmit={true}
+                               multiline={true}
+                               minHeight={60}
+                               maxHeight={200}
+                               autoCorrect={true}
+                           />
+                       </View>
+                    <View style={styles.buttonView}>
+                       <View style={styles.lineDivider}/>
+                        <TouchableOpacity onPress={replyButton}>
+                          <Text style={styles.button}>Submit Reply</Text>
+                        </TouchableOpacity>
+                       </View>
                    </View>
                </View>
            </View>
-       </View>
-    </View>
-  </ScrollView>
+        </View>
+      </KeyboardAwareScrollView>
   );
 }
 
